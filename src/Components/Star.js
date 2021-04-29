13ef98873b449f82df9d80e87691d7788339e1fd
D-DF <daviddefreitas0192@gmail.com>
2021-04-29 13:36:01 +0200

    ok

--- src/Components/Star.js
+++ src/Components/Star.js
@@ -4,12 +4,16 @@ const Star = () => {
     const [planet, setPlanets]  = useState([])
     const [loading, setLoading] = useState(false)
     const [apiCall, setApiCall] = useState(0)
-    const [error, setError]     = useState("")
+    const [page, setPage]       = useState(1)
+
+    console.log(apiCall)
+    const addPlanet = () => {
+        setPage(page +1)
+    }
     
     useEffect(() => {
         setLoading(true)
-        const url = `https://swapi.dev/api/planets`;
-
+        const url = `https://swapi.dev/api/planets/?page=${page}`;
         fetch(url)
             .then((response) => {
                 setApiCall(a => a + 1)
@@ -19,28 +23,23 @@ const Star = () => {
                 return response.json()
             })
             .then((data) => {
-                console.log(data)
-                setPlanets(data.results)
+                setLoading(false)
+                setPlanets(planet => [...planet, ...data.results])
             })
             .catch((error) => {
-                setError(error.message)
+                alert(error.message)
             })
-            .finally(() => {
-                setLoading(false)
-            })
-    }, [])
+            
+    }, [page])
     
-    
-
     return (
         <section className="container py-5" >
             <h1 className="mb-5">Planètes dans l'univers de Star Wars</h1>
-            {loading && <p className="h1">Loading...</p>}
         <div className="row">
             {planet.map((el) => {
                 return (
                     <div className="col-md-6 col-lg-4 col-xl-3 mb-4" key={el.name}>
-                        <article className="bg-warning p-3">
+                        <article className="p-3 planete">
                             <h2 className="h5">{el.name}</h2>
                             <p className="mb-0"><b>Population:</b> <br/>{el.population}</p> 
                             <p className="mb-0"><b>Climat:</b> <br/>{el.climate}</p> 
@@ -48,12 +47,11 @@ const Star = () => {
                     </div>   
                 )
             })}
-            
+            {loading &&<p>Loading...</p>}
         </div>
-        <button type="button" className="btn btn-dark">Suivantes</button>
+        {page > 6? <p className="bg-light w-100%">Voici la liste des planètes dans starWars</p> : <button type="button" className="btn btn-light" onClick={addPlanet}>Next</button>}
         </section>
     )
-    
 };
 
 export default Star;
